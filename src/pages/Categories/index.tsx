9df34c36dcabcc30c434e9b51745e6806c738476
JuanGuimaraes <juandasmerces@outlook.com>
2024-11-16 17:40:15 -0300

    continuaçao do projeto

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,66 @@
+import { Game } from '../Home'
+
+import ProductsList from '../../components/ProductsList'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import starWars from '../../assets/images/star_wars.png'
+import { useEffect, useState } from 'react'
+
+import {
+  useGetActionGamesQuery,
+  useGetFightGamesQuery,
+  useGetRpgGamesQuery,
+  useGetSimulationGamesQuery,
+  useGetSportGamesQuery
+} from '../../services/api'
+
+const Categories = () => {
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: fightGames } = useGetFightGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
+  const { data: sportGames } = useGetSportGamesQuery()
+  const { data: simulationGames } = useGetSimulationGamesQuery()
+
+  if (actionGames && fightGames && rpgGames && sportGames && simulationGames) {
+    return (
+      <>
+        <ProductsList
+          games={actionGames}
+          title="Ação"
+          background="black"
+          id="action"
+        />
+        <ProductsList
+          games={sportGames}
+          title="Esportes"
+          background="gray"
+          id="sport"
+        />
+        <ProductsList
+          games={fightGames}
+          title="Luta"
+          background="gray"
+          id="fight"
+        />
+        <ProductsList
+          games={rpgGames}
+          title="RPG"
+          background="black"
+          id="rpg"
+        />
+        <ProductsList
+          games={simulationGames}
+          title="Simulação"
+          background="black"
+          id="simulation"
+        />
+      </>
+    )
+  }
+
+  return <h4>Carregando...</h4>
+}
+
+export default Categories
